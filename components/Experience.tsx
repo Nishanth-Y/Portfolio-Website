49bce8cfdc74dd156b270137feb22a2d4ac0518b
Nishanth Y <70255477+Nishanth-Y@users.noreply.github.com>
2024-03-27 11:43:59 +0530

    new experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,6 +10,33 @@ const Experience = () => {
       <div className=" container mx-auto 2xl ">
         <div className="flex  flex-row justify-between pt-5">
           <p className="text-gray-300 ">
+            <span className="font-semibold">MedTourEasy /</span>{" "}
+            Data Analyst Trainee
+          </p>
+          <p className="text-gray-300">MAR 2023 - PRESENT, REMOTE</p>
+        </div>
+        <p className="text-gray-300 pt-5">
+        Currently working as Data Analytics Trainee for the Traineeship Program with MedTourEasy. My training is scheduled effective for a period of 4 weeks, where I gained experience with data analytics process and tools used and experience in real–time healthcare analysis project.
+          <br />
+        </p>
+        <div className="flex-col flex sm:flex-row ">
+          <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Python
+          </div>
+          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Tableau
+          </div>
+          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Data Analysis
+          </div>
+          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            R programming
+          </div>
+        </div>
+      </div>
+      <div className=" container mx-auto 2xl ">
+        <div className="flex  flex-row justify-between pt-5">
+          <p className="text-gray-300 ">
             <span className="font-semibold">Parheeksha Business Plugger /</span>{" "}
             Full Stack Web Dev Intern
           </p>
@@ -36,6 +63,7 @@ const Experience = () => {
           </div>
         </div>
       </div>
+      
     </section>
   );
 };
