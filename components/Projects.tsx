b7b6d3e2669366fafbcfef9c6625448c18c0b8db
Nishanth-Y <nishanthy1411@gmail.com>
2024-06-26 11:03:33 +0530

    fixed project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,27 +10,23 @@ const Projects: React.FC<{}> = () => {
         PROJECTS
       </h1>
       <div className=" container mx-auto 2xl  ">
-        <div className="flex-col flex md:flex-row  mt-7">
+        <div className="flex flex-col mt-7">
           <Link
             href="https://github.com/Nishanth-Y/Analyze-Death-Age-Difference-of-Right-Handers-with-Left-Handers-By-Nishanth-Y/blob/main/README.md"
             rel="noopener noreferrer"
             target="_blank"
             className="z-[1]"
           >
-            <div className="flex-row flex mb-5">
-              <Image
-                src="/SecondProject.jpg"
-                height={155}
-                width={155}
-                alt="UNI-ZONE Project"
-              />
+            <div className="flex-row flex mb-7">
+              <Image src="/DataFull.jpg" height={130} width={130} alt="MOVIE" />
               <div className="p-3">
                 <p className="text-white font-semibold text-xl">
-                 Analyze Death Age Difference of Right Handers with Left Handers
+                  Death Difference Of Right Handers With Left Handers
                 </p>
-                <p className="text-gray-500 text-[10px]">
-                This project proposal describes a data analysis effort to look at whether left-handed and right-
-                handed people die at different ages on average.
+                <p className="text-gray-500 text-[13px]">
+                  This project proposal describes a data analysis effort to look
+                  at whether left-handed and right- handed people die at
+                  different ages on average.
                 </p>
               </div>
             </div>
@@ -46,27 +42,25 @@ const Projects: React.FC<{}> = () => {
                 src="/FirstProject.jpeg"
                 height={130}
                 width={130}
-                alt="MOVIE"
+                alt="Social Media Clown"
               />
               <div className="p-3">
                 <p className="text-white font-semibold text-xl">Uni-Zone</p>
-                <p className="text-gray-500 text-[10px]">
-                  Implemented a Social Media Website Clone, for college clubs where club
-                  coordinator able to post updates regarding Events and
-                  Activities, this application developed using Django.
+                <p className="text-gray-500 text-[13px]">
+                  Implemented a Social Media Website Clone, for college clubs
+                  where club coordinator able to post updates regarding Events
+                  and Activities, this application developed using Django.
                 </p>
               </div>
             </div>
           </Link>
-        </div>
-        <div className="flex justify-center md:flex-row ">
           <Link
             href="https://github.com/Nishanth-Y/Portfolio-Manager"
             rel="noopener noreferrer"
             target="_blank"
             className="z-[1]"
           >
-            <div className="flex-row flex md:ml-40 mr-0">
+            <div className="flex-row flex mb-5">
               <Image
                 src="/ThridProject.jpg"
                 height={130}
@@ -77,7 +71,7 @@ const Projects: React.FC<{}> = () => {
                 <p className="text-white font-semibold text-xl">
                   Portfolio Manager
                 </p>
-                <p className="text-gray-500 text-[10px]">
+                <p className="text-gray-500 text-[13px]">
                   Implemented a web application, where users can provide details
                   and a portfolio is been generated with username endpoint
                 </p>
